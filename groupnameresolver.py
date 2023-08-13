53977e226de2c513a41c3e3ce5925ac1bd148d58
Ofek Harpaz <111492589+harpazofek@users.noreply.github.com>
2023-08-13 21:59:29 +0300

    Create groupnameresolver.py

---
+++ groupnameresolver.py
@@ -0,0 +1,14 @@
+# This module will contain the GroupNameResolver class
+class GroupNameResolver:
+    def __init__(self):
+        self.prefix_to_group = {
+            "GR": "Something",
+            "LR": "AnotherGroup",
+            "TP": "YetAnotherGroup"
+        }
+    
+    def determine_group_name(self, name):
+        for prefix, group_name in self.prefix_to_group.items():
+            if name.startswith(prefix):
+                return group_name
+        return "DefaultGroup"
